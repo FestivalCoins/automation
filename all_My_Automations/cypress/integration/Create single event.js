d62d8a0a59f75f4e284039c63adc7ceeb692a8ff
Nekaa0 <nekaabisola@gmail.com>
2021-06-28 14:45:02 +0100

    refactor

--- all_My_Automations/cypress/integration/Create single event.js
+++ all_My_Automations/cypress/integration/Create single event.js
@@ -28,15 +28,16 @@ describe('My First Test suite', () => {
     cy.get('.first--child > .el-form-item__content > .wrap > .el-row > .el-col-14 > .el-date-editor > .el-input__inner').type('May 24,2022')
     cy.get('.first--child > .el-form-item__content > .wrap > .el-row > .el-col-10 > .el-date-editor > .el-input__inner').type('00:00')
     cy.wait(1000)
-    cy.get(':nth-child(2) > .el-form-item__content > .wrap > .el-row > .el-col-14 > .el-date-editor > .el-input__inner').clear().type('May 24,2022')
-    cy.get(':nth-child(2) > .el-form-item__content > .wrap > .el-row > .el-col-10 > .el-date-editor > .el-input__inner').clear().type('19:00')
+    cy.get(':nth-child(2) > .el-form-item__content > .wrap > .el-row > .el-col-14 > .el-date-editor > .el-input__inner').clear().type('May 29,2022')
+    cy.get(':nth-child(2) > .el-form-item__content > .wrap > .el-row > .el-col-10 > .el-date-editor > .el-input__inner').clear().type('18:00')
     cy.get('.last--section > .event__form--section__content > .first--child > .el-form-item__content > .el-input > .el-input__inner').type("www.cnn.com")
     cy.get('[native-type="submit"]').click()
     cy.wait(2000)
     cy.contains('online events').should('be.visible')
+
     cy.get('.online--form__input').click()
     cy.contains("manually").click()
-    cy.get('.el-textarea__inner').type("Zoomlinkplaceholdertext")
+    cy.get('.el-textarea__inner').type("Zoomlinkplaceholdertexts")
     cy.get('.el-button').click()
     
 
