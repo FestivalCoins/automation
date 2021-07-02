9cea361e1307b405179ed9372c523cb3452e387f
Nekaa0 <nekaabisola@gmail.com>
2021-07-02 06:15:52 +0100

    added live events

--- all_My_Automations/cypress/integration/Recurring live event.js
+++ all_My_Automations/cypress/integration/Recurring live event.js
@@ -1,6 +1,6 @@
 // <reference types="cypress" />
 describe('My First Test suite', () => {
-    it('Attempt recurring event', () => {
+    it('login, create live event, create ticket and publish', () => {
     cy.visit("https://tix-staging.netlify.app/login/")
     //cy.get(':nth-child(5) > .u-button').click()
     cy.get('.login--form__email--input > .el-input__inner').type('nneka@tix.africa')
@@ -9,15 +9,16 @@ describe('My First Test suite', () => {
     cy.wait(2000) // wait for landing page
     cy.get('.events--page__header--text').should('be.visible')
     cy.get('span').click()
-    cy.get('.event-type__personal > .event-type__step').click()
+    cy.get('.event-type__business > .event-type__step').click()
     cy.get('.event-type__buttons > :nth-child(2)').click()
     const eventname = () => Cypress._.random(0, 1e2)
     const event = eventname()
-    const name = `Automated_recurring_event${event}`
+    const name = `Auto_Live_recurring_event${event}`
     const customname = `aut${event}`
     cy.get(':nth-child(1) > .event__form--section__content > .first--child > .el-form-item__content > .el-input > .el-input__inner').type(name)
     cy.get('.trix-content').type('just a description')
-    cy.get(':nth-child(1) > .event__form--section__content > :nth-child(3) > .el-form-item__content > .el-input > .el-input__inner').type(customname)
+    cy.get('.el-col-16 > .el-input > .el-input__inner').type("Hidden location to be revealed a day before the event")
+    cy.get(':nth-child(1) > .event__form--section__content > :nth-child(4) > .el-form-item__content > .el-input > .el-input__inner').type(customname)
     cy.get('.el-select__caret').click()
     cy.contains("career").should('be.visible')
     cy.contains('career').click()
@@ -32,14 +33,16 @@ describe('My First Test suite', () => {
     cy.get(':nth-child(2) > .el-form-item > .el-form-item__content > .el-date-editor > .el-input__inner').clear().type('18:00')
     cy.get(':nth-child(4) > :nth-child(1) > .el-form-item > .el-form-item__content > .flex > .el-radio > .el-radio__input > .el-radio__inner').click()
     cy.get('.input-number > .el-input__inner').type("4")
-    
+    cy.get('.last--section > .event__form--section__content > .first--child > .el-form-item__content > .el-input > .el-input__inner').type("www.cnn.com")
     cy.get('[native-type="submit"]').click()
     cy.wait(2000)
-    cy.contains('online events').should('be.visible')
-
-    cy.get('.online--form__input').select("manually add an event link")
-    cy.get('.el-textarea__inner').type("Zoomlinkplaceholdertexts")
-    cy.get('.el-button').click()
+    cy.get('.create--ticket__button').click()
+    cy.get('.el-form-item__content > .el-input > .el-input__inner').type("Ticket one")
+    cy.get('.el-textarea__inner').type('Description of the ticket')
+    cy.get(':nth-child(6) > .el-form-item__content > .el-row > .el-col-14 > .el-input-number > .el-input > .el-input__inner').type("100")
+    cy.get('[native-type="submit"]').click()
+    cy.get('.el-switch__core').click()
+   
     
 
 })
